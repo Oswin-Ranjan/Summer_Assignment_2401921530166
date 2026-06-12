b3eb646a9cefb88f11d26227474bb7df08e7699d
Oswin Ranjan <ranjanoswin@gmail.com>
2026-06-12 20:06:55 +0530

    Week_2 - OOPS Done

---
+++ Week_2/OOPS/src/live/Test.java
@@ -0,0 +1,26 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+  public static void main(String[] args) {
+    // a. Create an instance of Veena and call play() method
+    Veena veena=new Veena();
+    veena.play();
+
+    // b. Create an instance of Saxophone and call play() method
+    Saxophone saxophone=new Saxophone();
+    saxophone.play();
+
+    // c. Place the above instances in a variable of type Playable and then call play() 
+    Playable p;
+
+    p=new Veena();
+    p.play();
+
+    p=new Saxophone();
+    p.play();
+  }
+}
