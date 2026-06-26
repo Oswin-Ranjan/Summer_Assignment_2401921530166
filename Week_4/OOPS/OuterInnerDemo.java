7e98451f5656902ba103cf7b6db739ed62473b06
Oswin Ranjan <ranjanoswin@gmail.com>
2026-06-26 10:06:44 +0530

    Week_4 - OOPS Done

---
+++ Week_4/OOPS/OuterInnerDemo.java
@@ -0,0 +1,21 @@
+class Outer {
+    public void display() {
+        System.out.println("Display method of outer class");
+    }
+
+    class inner {
+        public void display() {
+            System.out.println("Display method of inner class");
+        }
+    }
+}
+
+public class OuterInnerDemo {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.display();
+
+        Outer.inner innerObject = outer.new inner();
+        innerObject.display();
+    }
+}
